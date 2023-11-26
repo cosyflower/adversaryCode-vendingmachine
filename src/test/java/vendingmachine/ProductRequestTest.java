7610c5dc7df2748cf932b55e2e74426a54c7f66f
sunghun <nokdonwithkj211@gmail.com>
2023-11-27 08:33:04 +0900

    test(ProductRequestTest): 등록할 상품의 형식에 맞게 작성했는지 검증

--- src/test/java/vendingmachine/ProductRequestTest.java
+++ src/test/java/vendingmachine/ProductRequestTest.java
@@ -1,2 +1,32 @@
-package vendingmachine;public class ProductRequestTest {
+package vendingmachine;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import vendingmachine.request.ProductRequest;
+
+public class ProductRequestTest {
+    @Nested
+    class 성공 {
+        @Test
+        void flow() {
+            Assertions.assertThatCode(() -> {
+                new ProductRequest("[짜파게티,1000,1]");
+                new ProductRequest("[짜파게티,1000,1];[자장면,2000,20]");
+            }).doesNotThrowAnyException();
+        }
+    }
+
+    @Nested
+    class 예외 {
+        @Test
+        void flow() {
+            Assertions.assertThatCode(() -> {
+                        new ProductRequest("[짜파게티,1000 1]");
+                        new ProductRequest("[짜파게티,1000,1][자장면,2000,20]");
+                    }).isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("[ERROR]");
+        }
+
+    }
 }
