77ee21fa6dbae961a32f974e2d813c90d069f4ec
sunghun <nokdonwithkj211@gmail.com>
2023-11-27 08:38:49 +0900

    feat(VendingMachineException): 자판기 관련 에러를 처리하는 클래스 생성:

--- src/main/java/vendingmachine/exception/VendingMachineException.java
+++ src/main/java/vendingmachine/exception/VendingMachineException.java
@@ -1,2 +1,8 @@
-package vendingmachine.exception;public class VendingMachineException {
+package vendingmachine.exception;
+
+public class VendingMachineException extends IllegalArgumentException {
+    private static final String ERROR_PREFIX = "[ERROR]";
+    public VendingMachineException(String errorMessage) {
+        super(ERROR_PREFIX + errorMessage);
+    }
 }
