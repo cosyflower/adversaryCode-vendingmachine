b40f4157cbf981fc21981b477906210731c89d76
sunghun <nokdonwithkj211@gmail.com>
2023-11-26 17:51:36 +0900

    feat(ChangeRequest): 잔돈 금액을 검증하는 클래스 생성

---
+++ src/main/java/vendingmachine/purchase/PurchaseMoney.java
@@ -0,0 +1,2 @@
+package vendingmachine.purchase;public class PurchaseMoney {
+}
--- src/main/java/vendingmachine/request/ChangeRequest.java
+++ src/main/java/vendingmachine/request/ChangeRequest.java
@@ -1,2 +1,16 @@
-package vendingmachine.request;public class ChangeRequest {
+package vendingmachine.request;
+
+public class ChangeRequest {
+    private final String changeRequestInput;
+
+    public ChangeRequest(String changeRequestInput) {
+        validateChangeRequest(changeRequestInput);
+        this.changeRequestInput = changeRequestInput;
+    }
+
+    private void validateChangeRequest(String changeRequestInput) {
+        // 1차 검증을 진행한다
+    }
+
+
 }
