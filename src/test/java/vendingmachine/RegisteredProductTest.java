7c1ef61b7b97f803208b16ca9ef81d6d43dbcc49
sunghun <nokdonwithkj211@gmail.com>
2023-11-27 08:34:17 +0900

    test(RegisteredProductTest): 상품들 중 중복된 이름이 존재하는지를 검증하는 테스트

--- src/test/java/vendingmachine/RegisteredProductTest.java
+++ src/test/java/vendingmachine/RegisteredProductTest.java
@@ -1,2 +1,34 @@
-package vendingmachine;public class RegisteredProductTest {
+package vendingmachine;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import vendingmachine.request.ProductRequest;
+
+public class RegisteredProductTest {
+    @Nested
+    class 성공 {
+        @Test
+        void 해결() {
+            Assertions.assertThatCode(() -> {
+                new ProductRequest("[짜파게티,1000,1]").toVendingProducts();
+                new ProductRequest("[짜파게티,1000,1];[자장면,2000,20]").toVendingProducts();
+            }).doesNotThrowAnyException();
+        }
+    }
+
+    @Nested
+    class 실패 {
+        @Test
+        void 예외() {
+            Assertions.assertThatCode(() -> {
+                        new ProductRequest("[짜파게티,1000,1];").toVendingProducts();
+                        new ProductRequest("[짜파게티,1000,1]=[자장면,2000,20]").toVendingProducts();
+                        new ProductRequest("[짜파게티,1000,1];[짜파게티,2000,20]").toVendingProducts();
+                }).isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("[ERROR]");
+        }
+
+    }
+
 }
