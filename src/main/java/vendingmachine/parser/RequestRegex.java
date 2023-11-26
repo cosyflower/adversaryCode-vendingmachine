a37f275678deff9fd83e080ad24992de6d94f61b
sunghun <nokdonwithkj211@gmail.com>
2023-11-27 08:36:54 +0900

    feat(RequestRegex): 검증 과정에서 필요한 상수 관리하는 클래스 생성

--- src/main/java/vendingmachine/parser/RequestRegex.java
+++ src/main/java/vendingmachine/parser/RequestRegex.java
@@ -1,2 +1,10 @@
-package vendingmachine.parser;public class RequestRegex {
+package vendingmachine.parser;
+
+import java.util.regex.Pattern;
+
+public class RequestRegex {
+    public static final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]+$");
+    public static final Pattern ORDER_PATTERN =
+            Pattern.compile(("(\\[[ㄱ-ㅎ가-힣]+,\\d+,\\d+\\])+(;\\[[ㄱ-ㅎ가-힣]+,\\d+,\\d+\\])*"));
+    public static final Pattern PRODUCT_PATTERN = Pattern.compile("^[ㄱ-ㅎ가-힣]+$");
 }
