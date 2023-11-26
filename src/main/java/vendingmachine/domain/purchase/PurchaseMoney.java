9182e1e4b66bea1780aa167b0e46021bd224abc2
sunghun <nokdonwithkj211@gmail.com>
2023-11-27 08:44:46 +0900

    feat(): 투입 금액, 구매하려는 상품을 관리하는 클래스를 각각 생성

--- src/main/java/vendingmachine/domain/purchase/PurchaseMoney.java
+++ src/main/java/vendingmachine/domain/purchase/PurchaseMoney.java
@@ -1,4 +1,22 @@
-package vendingmachine.purchase;
+package vendingmachine.domain.purchase;
+
+import vendingmachine.exception.StartsWithZeroException;
 
 public class PurchaseMoney {
+    private final int purchaseMoneyValue;
+
+    private PurchaseMoney(String purchaseMoneyValue) {
+        validatePurchaseMoney(purchaseMoneyValue);
+        this.purchaseMoneyValue = Integer.parseInt(purchaseMoneyValue);
+    }
+
+    private void validatePurchaseMoney(String purchaseMoneyValue) {
+        if (purchaseMoneyValue.startsWith("0") && purchaseMoneyValue.length() > 1) {
+            throw new StartsWithZeroException("0으로 시작하는 수를 입력했습니다");
+        }
+    }
+
+    public static PurchaseMoney from(String purchaseMoneyValue) {
+        return new PurchaseMoney(purchaseMoneyValue);
+    }
 }
--- src/main/java/vendingmachine/domain/purchase/PurchaseProduct.java
+++ src/main/java/vendingmachine/domain/purchase/PurchaseProduct.java
@@ -1,6 +1,28 @@
 package vendingmachine.domain.purchase;
 
-public class PurchaseProducts {
-    // 단일 상품만 입력하는 방향으로
-    // 존재하는 메뉴인지 확인하는 과정은 상위 클래스에서 진행하는 것으로 하자
+import vendingmachine.domain.VendingMachine;
+import vendingmachine.exception.VendingMachineException;
+
+public class PurchaseProduct {
+    private final String purchaseProductName;
+
+    private PurchaseProduct(String purchaseProductName, VendingMachine vendingMachine) {
+        validatePurchaseProduct(purchaseProductName, vendingMachine);
+        this.purchaseProductName = purchaseProductName;
+    }
+
+    public static PurchaseProduct of(String purchaseProductName, VendingMachine vendingMachine) {
+        return new PurchaseProduct(purchaseProductName, vendingMachine);
+    }
+
+    private void validatePurchaseProduct(String purchaseProductName, VendingMachine vendingMachine) {
+        if (vendingMachine.hasProduct(purchaseProductName)) {
+            return;
+        }
+        throw new VendingMachineException("존재하지 않는 상품입니다.");
+    }
+
+    public String getPurchaseProductName() {
+        return purchaseProductName;
+    }
 }
