cf1b13a74fd1e5d1f41d960e0d5d019ca176a4aa
sunghun <nokdonwithkj211@gmail.com>
2023-11-27 08:36:00 +0900

    feat(RegisteredProduct): 상품 관련 정보를 관리하는 클래스 생성, 상품들을 관리하는 일급 컬렉션 생성

--- src/main/java/vendingmachine/domain/vending/product/RegisteredProduct.java
+++ src/main/java/vendingmachine/domain/vending/product/RegisteredProduct.java
@@ -1,17 +1,24 @@
-package vendingmachine.product;
+package vendingmachine.domain.vending.product;
 
 public class RegisteredProduct {
     private final ProductName productName;
     private final ProductPrice productPrice;
     private final ProductQuantity productQuantity;
 
-    public RegisteredProduct(ProductName productName, ProductPrice productPrice,
+    private RegisteredProduct(ProductName productName, ProductPrice productPrice,
                              ProductQuantity productQuantity) {
-        // 검증은 각각의 클래스 내에서 진행한다 -> 다시 말해, 이 구간에서는 검증할 이유가 없음
-        // 검증하지 않아도 된다
         this.productName = productName;
         this.productPrice = productPrice;
         this.productQuantity = productQuantity;
+    }
+
+    public static RegisteredProduct of(ProductName productName, ProductPrice productPrice,
+                                       ProductQuantity productQuantity) {
+        return new RegisteredProduct(productName, productPrice, productQuantity);
+    }
+
+    public boolean hasProduct(String purchaseProductValue) {
+        return productName.hasSameName(purchaseProductValue);
     }
 
     public ProductName getProductName() {
--- src/main/java/vendingmachine/domain/vending/product/VendingProducts.java
+++ src/main/java/vendingmachine/domain/vending/product/VendingProducts.java
@@ -1,16 +1,22 @@
-package vendingmachine.product;
+package vendingmachine.domain.vending.product;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import vendingmachine.domain.purchase.PurchaseProduct;
+import vendingmachine.exception.ProductException;
 
 public class VendingProducts {
     // 실제로 관리하는 상품들
     private final List<RegisteredProduct> vendingProducts;
 
-    public VendingProducts(List<RegisteredProduct> vendingProducts) {
-        // 중복된 이름인지 검증해야 한다
+    private VendingProducts(List<RegisteredProduct> vendingProducts) {
         validateVendingProducts(vendingProducts);
         this.vendingProducts = new ArrayList<>(vendingProducts);
+    }
+
+    public static VendingProducts from(List<RegisteredProduct> convertedProduct) {
+        return new VendingProducts(convertedProduct);
     }
 
     private void validateVendingProducts(List<RegisteredProduct> vendingProducts) {
@@ -18,8 +24,28 @@ public class VendingProducts {
     }
 
     private void hasDuplicatedName(List<RegisteredProduct> vendingProducts) {
+        List<ProductName> productNames = vendingProducts.stream()
+                .map(RegisteredProduct::getProductName)
+                .collect(Collectors.toList());
 
+        if (vendingProducts.size() == productNames.stream().distinct().count()) {
+            return;
+        }
+        throw new ProductException("중복된 상품을 입력했습니다.");
     }
 
 
+    public boolean isProductExisted(String purchaseProductNameValue) {
+        return vendingProducts.stream()
+                .filter(vendingProduct -> vendingProduct.hasProduct(purchaseProductNameValue)) // 추출하기
+                .findAny().isPresent();
+    }
+
+    public RegisteredProduct findMatchedProduct(PurchaseProduct purchaseProduct) {
+        return vendingProducts.stream()
+                .filter(registeredProduct ->
+                        registeredProduct.hasProduct(purchaseProduct.getPurchaseProductName())) // 추출하기
+                .findFirst()
+                .get();
+    }
 }
