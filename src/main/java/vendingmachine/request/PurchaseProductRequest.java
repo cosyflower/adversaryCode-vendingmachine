611b95bc97343e807832e215d6ca5ac9fa2c213d
sunghun <nokdonwithkj211@gmail.com>
2023-11-27 17:05:42 +0900

    test(PurchaseProductRequestTest): 상품의 형식에 맞는지 검증하는 테스트 작성

--- src/main/java/vendingmachine/request/PurchaseProductRequest.java
+++ src/main/java/vendingmachine/request/PurchaseProductRequest.java
@@ -1,9 +1,37 @@
 package vendingmachine.request;
 
-public class PurchaseProductRequest {
-    private final String purchaseProductRequest;
+import vendingmachine.domain.VendingMachine;
+import vendingmachine.domain.purchase.PurchaseProduct;
+import vendingmachine.exception.ProductException;
+import vendingmachine.parser.RequestRegex;
 
-    public PurchaseProductRequest(String purchaseProductRequest) {
-        this.purchaseProductRequest = purchaseProductRequest;
+public class PurchaseProductRequest {
+    private final String purchaseProductRequestInput;
+
+    public PurchaseProductRequest(String purchaseProductRequestInput) {
+        validatePurchaseProductRequestInput(purchaseProductRequestInput);
+        this.purchaseProductRequestInput = purchaseProductRequestInput;
+    }
+
+    public PurchaseProduct toPurChaseProduct(VendingMachine vendingMachine) {
+        return PurchaseProduct.from(purchaseProductRequestInput, vendingMachine);
+    }
+
+    private void validatePurchaseProductRequestInput(String purchaseProductRequestInput) {
+        isNullOrEmpty(purchaseProductRequestInput);
+        hasProductPattern(purchaseProductRequestInput);
+    }
+
+    private void isNullOrEmpty(String purchaseProductRequestInput) {
+        if (purchaseProductRequestInput == null || purchaseProductRequestInput.isEmpty()) {
+            throw new ProductException("아무것도 입력하지 않았습니다.");
+        }
+    }
+
+    private void hasProductPattern(String purchaseProductRequestInput) {
+        if (RequestRegex.PRODUCT_PATTERN.matcher(purchaseProductRequestInput).matches()) {
+            return;
+        }
+        throw new ProductException("상품 형식에 맞지 않는 입력입니다.");
     }
 }
--- src/test/java/vendingmachine/purchase/PurchaseProductRequestTest.java
+++ src/test/java/vendingmachine/purchase/PurchaseProductRequestTest.java
@@ -1,2 +1,24 @@
-package vendingmachine.purchase;public class PurchaseProductRequestTest {
+package vendingmachine.purchase;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import vendingmachine.exception.ProductException;
+import vendingmachine.request.PurchaseProductRequest;
+
+public class PurchaseProductRequestTest {
+    @Test
+    void 성공() {
+        Assertions.assertThatCode(() -> new PurchaseProductRequest("짜파게티"))
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void 실패(String nothingInput) {
+        Assertions.assertThatCode(() -> new PurchaseProductRequest(nothingInput))
+                .isInstanceOf(ProductException.class)
+                .hasMessageContaining("[ERROR]");
+    }
 }
