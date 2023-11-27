b7130acc5e7809e6cf3033ad3ec061a10582fe76
sunghun <nokdonwithkj211@gmail.com>
2023-11-27 16:51:15 +0900

    feat(PurchaseProductRequest): 구매 가능한, 등록되어 있는 상품인지 검증하는 클래스 형성

--- src/main/java/vendingmachine/request/PurchaseProductRequest.java
+++ src/main/java/vendingmachine/request/PurchaseProductRequest.java
@@ -1,37 +1,9 @@
 package vendingmachine.request;
 
-import vendingmachine.domain.VendingMachine;
-import vendingmachine.domain.purchase.PurchaseProduct;
-import vendingmachine.exception.ProductException;
-import vendingmachine.parser.RequestRegex;
-
 public class PurchaseProductRequest {
-    private final String purchaseProduct;
+    private final String purchaseProductRequest;
 
-    public PurchaseProductRequest(String purchaseProduct) {
-        validatePurchaseProduct(purchaseProduct);
-        this.purchaseProduct = purchaseProduct;
-    }
-
-    public PurchaseProduct toPurchaseProduct(VendingMachine vendingMachine) {
-        return PurchaseProduct.of(purchaseProduct, vendingMachine);
-    }
-
-    private void validatePurchaseProduct(String purchaseProduct) {
-        isNullOrEmpty(purchaseProduct);
-        hasProductPattern(purchaseProduct);
-    }
-
-    private void isNullOrEmpty(String purchaseProduct) {
-        if (purchaseProduct == null || purchaseProduct.isEmpty()) {
-            throw new ProductException("아무것도 입력하지 않았습니다");
-        }
-    }
-
-    private void hasProductPattern(String purchaseProduct) {
-        if (RequestRegex.PRODUCT_PATTERN.matcher(purchaseProduct).matches()) {
-            return;
-        }
-        throw new ProductException("상품 정보를 잘못 입력했습니다.");
+    public PurchaseProductRequest(String purchaseProductRequest) {
+        this.purchaseProductRequest = purchaseProductRequest;
     }
 }
--- src/test/java/vendingmachine/purchase/PurchaseProductRequestTest.java
+++ src/test/java/vendingmachine/purchase/PurchaseProductRequestTest.java
@@ -1,13 +1,2 @@
-package vendingmachine.purchase;
-
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import vendingmachine.request.PurchaseProductRequest;
-
-public class PurchaseProductRequestTest {
-    @Test
-    void 성공() {
-        Assertions.assertThatCode(() -> new PurchaseProductRequest("자파게티"))
-                .doesNotThrowAnyException();
-    }
+package vendingmachine.purchase;public class PurchaseProductRequestTest {
 }
